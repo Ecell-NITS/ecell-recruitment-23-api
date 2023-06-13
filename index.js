b52efa01e5ce711943863e0255c7925c4e6d574a
hereisadi <hereisadi@proton.me>
2023-06-13 23:34:43 +0530

    otp error fix

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const session = require('express-session');
 const mongoose = require("mongoose");
 const UserModel = require("./models/Users");
 const bodyParser = require("body-parser");
@@ -7,8 +8,18 @@ const nodemailer = require("nodemailer");
 app.use(bodyParser.json());
 const cors = require("cors");
 require("dotenv").config();
+const crypto = require('crypto');
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://ecellnits.org',
+  // origin: 'http://localhost:3000',
+  credentials: true
+}));
+app.use(session({
+  secret: crypto.randomBytes(32).toString('hex'),
+  resave: false,
+  saveUninitialized: true
+}));
 
 mongoose.connect(process.env.MONGODBSECRET);
 
@@ -99,7 +110,8 @@ app.post("/send-otp", async (req, res) => {
   try {
     sendEmail(email, "ECELL OTP Verification", `Your OTP for verifying your email id for filling Ecell recuitment form is: ${otp}`);
 
-    storedOTP = otp.toString();
+    req.session.otp = otp.toString();
+
     res.json({ success: true, otp });
   } catch (error) {
     console.log("Error sending OTP:", error);
@@ -110,15 +122,20 @@ app.post("/send-otp", async (req, res) => {
 app.post("/verify-otp", (req, res) => {
   console.log("Request Body:", req.body);
   const enteredOTP = req.body.otp.toString().trim();
-  const storedOTPString = storedOTP.toString().trim();
+  const storedOTPString = req.session.otp;
 
   console.log("Entered OTP:", enteredOTP);
   console.log("Stored OTP:", storedOTPString);
 
-  if (enteredOTP === storedOTPString) {
-    res.status(200).json({ message: "OTP verified successfully" });
+  if (req.session.otp) {
+    if (enteredOTP === storedOTPString) {
+      delete req.session.otp;
+      res.status(200).json({ message: "OTP verified successfully" });
+    } else {
+      res.status(400).json({ message: "Wrong OTP. Please try again" });
+    }
   } else {
-    res.status(400).json({ message: "Wrong OTP. Please try again" });
+    res.status(400).json({ message: "No OTP found. Please generate a new OTP" });
   }
 });
 
